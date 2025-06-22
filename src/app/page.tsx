a0096b5e78dee0af6297076170f3ba9b329da0ab
DevPhai <thapana10@gmail.com>
2025-06-22 23:41:40 +0700

    add l margin and new bg color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-start justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div
+      className="grid grid-rows-[20px_1fr_20px] items-start justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
+      style={{ marginLeft: "80px" }}
+    >
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1>HOME</h1>
         <Image
